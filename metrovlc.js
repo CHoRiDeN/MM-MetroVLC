3029725493c2c1ad277717280f1314dbdbc9e909
Dani Jimenez <dani.jimenez@bebetting.io>
2022-09-29 13:16:27 +0200

    fixed

--- metrovlc.js
+++ metrovlc.js
@@ -10,8 +10,8 @@ Module.register("metrovlc", {
 		text: "Metro VLC",
 		useRealtime: true,
 		updateInterval: 30 * 1000, // Update 30 secs
-		stationFrom: 122,
-		stationTo: 182
+		stationFrom: 121,
+		stationTo: 108
 	},
 	getHeader: function () {
 		if (this.origin == null || this.destination == null) {
@@ -29,11 +29,12 @@ Module.register("metrovlc", {
 		this.scheduleUpdate();
 	},
 	scheduleUpdate: function () {
-		clearTimeout(self.updateTimer);
+		var self = this;
+		clearTimeout(this.updateTimer);
 		self.updateTimer = setTimeout(function () {
 			console.log("sending noti");
-			self.sendSocketNotification("GETDATA", this.config);
-		}, self.config.updateInterval);
+			self.sendSocketNotification("GETDATA", self.config);
+		}, this.config.updateInterval);
 	},
 
 	getDom: function () {
@@ -72,7 +73,7 @@ Module.register("metrovlc", {
 	},
 	// using the results retrieved for the API call
 	socketNotificationReceived: function (notification, payload) {
-		const self = this;
+
 		console.log(notification);
 		console.log(payload);
 		if (notification === "TRANSPORTS") {
@@ -85,3 +86,4 @@ Module.register("metrovlc", {
 		}
 	}
 });
+
